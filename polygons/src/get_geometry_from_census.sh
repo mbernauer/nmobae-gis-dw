1bac73639d91c6f131fb3c6f3509bfeb2116c690
Michael Bernauer <mlbernauer@gmail.com>
2023-10-11 18:07:39 -0600

    update

--- polygons/src/get_geometry_from_census.sh
+++ polygons/src/get_geometry_from_census.sh
@@ -5,9 +5,9 @@ function main () {
 	curl -s "https://www2.census.gov/geo/tiger/TIGER${YEAR}/STATE/tl_${YEAR}_us_state.zip" -o ${YEAR}_STATE.zip
 	curl -s "https://www2.census.gov/geo/tiger/TIGER${YEAR}/COUNTY/tl_${YEAR}_us_county.zip" -o ${YEAR}_COUNTY.zip
 	curl -s "https://www2.census.gov/geo/tiger/TIGER${YEAR}/TRACT/tl_${YEAR}_35_tract.zip" -o ${YEAR}_TRACT.zip
-	./src/extract_geometry.py ${YEAR}_STATE.zip STATE ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/' 
-	./src/extract_geometry.py ${YEAR}_COUNTY.zip COUNTY ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/' 
-	./src/extract_geometry.py ${YEAR}_TRACT.zip TRACT ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/'
+	extract_geometry.py ${YEAR}_STATE.zip STATE ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/' 
+	extract_geometry.py ${YEAR}_COUNTY.zip COUNTY ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/' 
+	extract_geometry.py ${YEAR}_TRACT.zip TRACT ${YEAR} | perl -F'\t' -lane 'print if $F[2] =~ /^35/'
 }
 
 if [[ $# -ne 1 ]]; then
